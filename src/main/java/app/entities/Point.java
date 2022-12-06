ccb55c586e043e007087fbd5fa3937df9459a6f0
Sobolev Ivan <sobolev1van@yandex.ru>
2022-12-06 19:01:06 +0300

    Add files via upload

---
+++ src/main/java/app/entities/Point.java
@@ -0,0 +1,30 @@
+package app.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "POINTZ")
+public class Point {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private Double x;
+    private Double y;
+    private Double r;
+    private String owner;
+    private Boolean checked;
+    private String currtime;
+    private Double extime;
+
+    public Point(Double x, Double y, Double r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+}
